c1ae010fca26e9c109e20fbb3f6c34f041169d16
Dmitro Korostelyov <admin@gmail.com>
2024-08-26 16:54:48 +0300

    Finifh

--- src/pack/Main.java
+++ src/pack/Main.java
@@ -1,7 +1,24 @@
 package pack;
+import pack.Worker.Address;
+import pack.Worker.Contacts;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        // Создание объектов класса Address и Contacts
+        Address address1 = new Address("Україна", "Дніпропетровська область", "місто Дніпро","проспект Олександра Поля", "98-Д", "45");
+        Contacts contacts1 = new Contacts("worker1@gmail.com", "+38 (050) 154 31 16");
+
+        Address address2 = new Address("Україна", "Київська область", "місто Київ","вулиця Бригадна", "11", "5");
+        Contacts contacts2 = new Contacts("worker2@gmail.com", "+38 (050) 316 47 45");
+
+        // Создание объектов класса Worker
+        Worker worker1 = new Worker(1, "Коростельов", "Дмитро", 36, 1, address1, contacts1);
+        Worker worker2 = new Worker(2, "Григоренко", "Ігор", 33, 5, address2, contacts2); // gender =  NOT_SPECIFIED, бо не попадає в даіпазон MIN MAX 0 - 2
+
+        // Вывод объектов в консоль
+        System.out.println(worker1);
+        System.out.println(worker2);
     }
 }
---
+++ src/pack/Worker.java
@@ -0,0 +1,232 @@
+package pack;
+
+//Клас Worker
+public class Worker {
+    //Поля класу Worker
+    private int id;
+    private String surname;
+    private String name;
+    private int age;
+    private int gender;
+    private Address address;
+    private Contacts contacts;
+
+    // Конструктор с параметрами
+    public Worker(int id, String surname, String name, int age, int gender, Address address, Contacts contacts) {
+        this.id = id;
+        this.surname = surname;
+        this.name = name;
+        this.age = age;
+        ///////
+        setGender(gender); // Используем сеттер для установки значения пола
+        this.address = address;
+        this.contacts = contacts;
+    }
+
+    // Геттеры и сеттеры
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        if (gender < Gender.MIN || gender > Gender.MAX) {
+            this.gender = Gender.NOT_SPECIFIED;
+        } else {
+            this.gender = gender;
+        }
+    }
+
+    public String getGenderAsString() {
+        switch (gender) {
+            case Gender.MALE:
+                return "MALE";
+            case Gender.FEMALE:
+                return "FEMALE";
+            case Gender.NOT_SPECIFIED:
+            default:
+                return "NOT_SPECIFIED";
+        }
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Contacts getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(Contacts contacts) {
+        this.contacts = contacts;
+    }
+
+
+    //Додаємо метод to String
+    @Override
+    public String toString() {
+        return "Worker #" + id + " (" +
+                "id = " + id +
+                "; Surname = " + surname +
+                "; Name = " + name  +
+                "; age = " + age +
+                "; gender = " + getGenderAsString() + ";" + "\n" +
+                "address = " + address + ";" + "\n"+
+                "contacts=" + contacts + ")" +  ")"+ "\n" ;
+    }
+
+    // Додаю вкладений клас Gender з набором статичних констант
+    public static class Gender {
+        public static final int NOT_SPECIFIED = 0;
+        public static final int MALE = 1;
+        public static final int FEMALE = 2;
+
+        public static final int MIN = NOT_SPECIFIED;
+        public static final int MAX = FEMALE;
+    }
+
+    // Додаємо вкладений клас Address з полями
+    public static class Address {
+        private String country;
+        private String region;
+        private String town;
+        private String street;
+        private String house;
+        private String apartment;
+
+        // Конструктор без параметрів
+        public Address() {
+        }
+
+        // Конструкторз усіма параметрами
+        public Address(String country, String region, String town, String street, String house, String apartment) {
+            this.country = country;
+            this.region = region;
+            this.town = town;
+            this.street = street;
+            this.house = house;
+            this.apartment = apartment;
+        }
+
+        // Геттеры и сеттеры
+        public String getCountry() {
+            return country;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+
+        public String getRegion() {
+            return region;
+        }
+
+        public void setRegion(String region) {
+            this.region = region;
+        }
+
+        public String getTown() {
+            return town;
+        }
+
+        public void setTown(String town) {
+            this.town = town;
+        }
+
+        public String getHouse() {
+            return house;
+        }
+
+        public void setHouse(String house) {
+            this.house = house;
+        }
+
+        public String getApartment() {
+            return apartment;
+        }
+
+        public void setApartment(String apartment) {
+            this.apartment = apartment;
+        }
+
+        // Перевизначення методу toString
+        @Override
+        public String toString() {
+            return country + ", " + region + ", " + town + ", " + street + ", " + house + "/" + apartment;
+        }
+    }
+
+    // Вложенный класс Contacts
+    public static class Contacts {
+        private String mail;
+        private String phone;
+
+        // Конструктор без параметров
+        public Contacts() {
+        }
+
+        // Конструктор с параметрами
+        public Contacts(String mail, String phone) {
+            this.mail = mail;
+            this.phone = phone;
+        }
+
+        // Геттеры и сеттеры
+        public String getMail() {
+            return mail;
+        }
+
+        public void setMail(String mail) {
+            this.mail = mail;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+
+        public void setPhone(String phone) {
+            this.phone = phone;
+        }
+
+        // Переопределение метода toString
+        @Override
+        public String toString() {
+            return "Email: " + mail + ", Phone: " + phone;
+        }
+    }
+}
